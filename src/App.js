0ca495f51797a29c0d709d820ee55f95022f584d
shabbir-umii <shabbir1993@users.noreply.github.com>
2024-08-06 19:57:02 +0500

    setup env, update git ignore, install axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 
 function App() {
+  const guardianApiKey = process.env.REACT_APP_GUARDIAN_API_KEY;
+  const nyTimesApiKey = process.env.REACT_APP_NY_TIMES_API_KEY;
+
+  console.log(`API Key: ${guardianApiKey}`);
+  console.log(`API URL: ${nyTimesApiKey}`);
   return (
     <div className="App">
       News Aggregator App
