3e6214a3a8696a5de5430f22cf2311bb4a08431f
shabbir-umii <shabbir1993@users.noreply.github.com>
2024-08-06 20:09:24 +0500

    install required dependencies for UI elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { useState } from "react";
 
 function App() {
   const guardianApiKey = process.env.REACT_APP_GUARDIAN_API_KEY;
   const nyTimesApiKey = process.env.REACT_APP_NY_TIMES_API_KEY;
 
-  console.log(`API Key: ${guardianApiKey}`);
-  console.log(`API URL: ${nyTimesApiKey}`);
+  const [articles, setArticles] = useState([]);
+  
   return (
     <div className="App">
       News Aggregator App
